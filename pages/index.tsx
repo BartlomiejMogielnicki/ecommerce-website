316595c8b23f8b4bc62e620d42218b5c6704b4c5
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-12-05 14:04:57 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import { FC } from 'react';
+
+const Home: FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Ecommerce website</title>
+      </Head>
+    </div>
+  );
+}
+
+export default Home;
