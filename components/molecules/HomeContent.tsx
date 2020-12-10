308045c814bbcf1f4002265d38bdd943e6b6cc05
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-12-10 14:47:11 +0100

    Add new home section - scroll to video onclick

--- components/molecules/HomeContent.tsx
+++ components/molecules/HomeContent.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import Link from 'next/link'
 import ReactPlayer from 'react-player'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './HomeContent.module.scss'
 
 const HomeContent:FC = () => (
   <div className={styles.wrapper}>
-    <div className={styles.container}>
+    <div className={styles.advContainer}>
       <div>
         <div className={`${styles.smallAdv} ${styles.topLeftAdv}`}>
           <Link href="/products/cameras">
@@ -52,10 +54,21 @@ const HomeContent:FC = () => (
         </div>
       </div>
     </div>
-    <div className={styles.videoSection}>
+    <div className={styles.advNavigateContainer}>
+      <a href="#videoSection">
+        <FontAwesomeIcon icon={faAngleDown} />
+        <p>Watch the trailer!</p>
+      </a>
+    </div>
+    <div className={styles.videoSection} id="videoSection">
       <div className={styles.videoContainer}>
         <ReactPlayer url="https://www.youtube.com/watch?v=xYX6b1-9Coo" controls width="100%" height="100%" />
       </div>
+      <button type="button">
+        <Link href="/products/cameras/gopro-9-black">
+          <a>GoPro HERO9 Black - Details</a>
+        </Link>
+      </button>
     </div>
   </div>
 )
