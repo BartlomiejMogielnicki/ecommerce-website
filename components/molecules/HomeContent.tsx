2636cd7a42d7ae346ed22f5413f1102d7635b9f2
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-12-10 13:37:50 +0100

    Add video player into homepage

--- components/molecules/HomeContent.tsx
+++ components/molecules/HomeContent.tsx
@@ -1,52 +1,60 @@
 import { FC } from 'react'
 import Link from 'next/link'
+import ReactPlayer from 'react-player'
 import styles from './HomeContent.module.scss'
 
 const HomeContent:FC = () => (
-  <div className={styles.container}>
-    <div>
-      <div className={`${styles.smallAdv} ${styles.topLeftAdv}`}>
-        <Link href="/products/cameras">
-          <a>
-            <img src="/home-cameras.jpeg" alt="" />
-            <p className={styles.advTitle}>Cameras</p>
+  <div className={styles.wrapper}>
+    <div className={styles.container}>
+      <div>
+        <div className={`${styles.smallAdv} ${styles.topLeftAdv}`}>
+          <Link href="/products/cameras">
+            <a>
+              <img src="/home-cameras.jpeg" alt="" />
+              <p className={styles.advTitle}>Cameras</p>
 
-          </a>
+            </a>
+          </Link>
+        </div>
+        <div className={`${styles.smallAdv} ${styles.botLeftAdv}`}>
+          <Link href="/products/mods">
+            <a>
+              <img src="/home-mods.jpeg" alt="" />
+              <p className={styles.advTitle}>Mods</p>
+
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.mainAdv}>
+        <Link href="/products/cameras/gopro-9-black">
+          <a><img src="/homehero9.jpg" alt="" /></a>
         </Link>
       </div>
-      <div className={`${styles.smallAdv} ${styles.botLeftAdv}`}>
-        <Link href="/products/mods">
-          <a>
-            <img src="/home-mods.jpeg" alt="" />
-            <p className={styles.advTitle}>Mods</p>
+      <div>
+        <div className={`${styles.smallAdv} ${styles.topRightAdv}`}>
+          <Link href="/products/protection">
+            <a>
+              <img src="/home-protection.jpg" alt="" />
+              <p className={styles.advTitle}>Protection</p>
 
-          </a>
-        </Link>
+            </a>
+          </Link>
+        </div>
+        <div className={`${styles.smallAdv} ${styles.botRightAdv}`}>
+          <Link href="/products/mounts">
+            <a>
+              <img src="/home-mounts.jpg" alt="" />
+              <p className={styles.advTitle}>Mounts</p>
+
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
-    <div className={styles.mainAdv}>
-      <Link href="/products/cameras/gopro-9-black">
-        <a><img src="/homehero9.jpg" alt="" /></a>
-      </Link>
-    </div>
-    <div>
-      <div className={`${styles.smallAdv} ${styles.topRightAdv}`}>
-        <Link href="/products/mounts">
-          <a>
-            <img src="/home-mounts.jpg" alt="" />
-            <p className={styles.advTitle}>Mounts</p>
-
-          </a>
-        </Link>
-      </div>
-      <div className={`${styles.smallAdv} ${styles.botRightAdv}`}>
-        <Link href="/products/protection">
-          <a>
-            <img src="/home-protection.jpg" alt="" />
-            <p className={styles.advTitle}>Protection</p>
-
-          </a>
-        </Link>
+    <div className={styles.videoSection}>
+      <div className={styles.videoContainer}>
+        <ReactPlayer url="https://www.youtube.com/watch?v=xYX6b1-9Coo" controls width="100%" height="100%" />
       </div>
     </div>
   </div>
