6c7f05abc6b4f45f937ca8126be2f4225aa4c9a4
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-12-11 11:14:56 +0100

    Add static content in bestsellers section. Link items with product details page

--- components/organisms/HomeContent/HomeBestsGallery/HomeBestsGallery.tsx
+++ components/organisms/HomeContent/HomeBestsGallery/HomeBestsGallery.tsx
@@ -1,15 +1,30 @@
 import { FC } from 'react'
+import Link from 'next/link'
 import styles from './HomeBestsGallery.module.scss'
 
 const bestsellers = [
   {
     imgSrc: '/best-gp8.jpg',
     title: 'GoPro 8 Black',
+    category: 'cameras',
+    price: '299.99$',
+  },
+  {
+    imgSrc: '/best-gp8.jpg',
+    title: 'GoPro 8 Black',
+    category: 'cameras',
     price: '299.99$',
   },
   {
     imgSrc: '/best-gp7.jpg',
     title: 'GoPro 7 Black',
+    category: 'cameras',
+    price: '249.99$',
+  },
+  {
+    imgSrc: '/best-gp7.jpg',
+    title: 'GoPro 7 Black',
+    category: 'cameras',
     price: '249.99$',
   },
 ]
@@ -20,9 +35,13 @@ const HomeBestsGallery:FC = () => (
     <ul className={styles.bestList}>
       {bestsellers.map((item) => (
         <li key={item.title} className={styles.bestItem}>
-          <img src={item.imgSrc} alt={item.title} />
-          <p><strong>{item.title}</strong></p>
-          <p>{item.price}</p>
+          <Link href={`/products/${item.category}/${item.title.split(' ').join('-').toLowerCase()}`}>
+            <a>
+              <img src={item.imgSrc} alt={item.title} />
+              <p><strong>{item.title}</strong></p>
+              <p>{item.price}</p>
+            </a>
+          </Link>
         </li>
       ))}
     </ul>
