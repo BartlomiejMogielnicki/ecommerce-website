0eba4e3feae3a268edf7258f108598647be2b677
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-12-17 13:24:40 +0100

    Recent posts styling

--- components/organisms/HomeContent/HomeRecentPosts/HomeRecentPosts.tsx
+++ components/organisms/HomeContent/HomeRecentPosts/HomeRecentPosts.tsx
@@ -13,12 +13,15 @@ const HomeRecentPosts: FC<Props> = ({ recentPosts }) => (
     <ul className={styles.postsList}>
       {recentPosts.map((item) => (
         <li key={item.title} className={styles.post}>
-          <Link href={`/blog/${item.title.split(' ').join('-').toLowerCase()}`}>
-            <a>
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
-            </a>
-          </Link>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+          <button type="button">
+            <Link href={`/blog/${item.title.split(' ').join('-').toLowerCase()}`}>
+              <a>
+                Read
+              </a>
+            </Link>
+          </button>
         </li>
       ))}
     </ul>
