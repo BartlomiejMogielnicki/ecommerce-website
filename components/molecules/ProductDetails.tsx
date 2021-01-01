50cae72da44d9db480298b870fbfd35d08c6caa6
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-01-01 12:37:40 +0100

    Create product image show modal state. Add open product image modal button

--- components/molecules/ProductDetails.tsx
+++ components/molecules/ProductDetails.tsx
@@ -1,7 +1,9 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Link from 'next/link'
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './ProductDetails.module.scss';
 import { Product } from '../../types';
 
@@ -9,36 +11,39 @@ interface Props {
   product: Product
 }
 
-const ProductDetails:FC<Props> = ({ product }) => (
-  <div className={styles.container}>
-    {/* <div className={styles.imageContainer}>
-      <img src={product.images[0].url} alt={product.title} />
-    </div> */}
-    <div className={styles.imageContainer}>
-      <Carousel className="carousel-style" showStatus={false} showArrows={false}>
-        {product.images.map((image) => (
-          <div className={styles.activeImage}>
-            <img src={image.url} alt={image.name} />
-          </div>
-        ))}
-      </Carousel>
-    </div>
-    <div className={styles.panel}>
-      <h1>{product.title}</h1>
-      <p className={styles.description}>{product.description}</p>
-      <p className={styles.price}>{product.price}</p>
-      <div className={styles.btnsContainer}>
-        <button type="button">
-          <Link href={`/products/${product.category}`}>
-            <a>{`Return to ${product.category}`}</a>
-          </Link>
-        </button>
-        <button type="button">
-          <p>Add to cart</p>
+const ProductDetails:FC<Props> = ({ product }) => {
+  const [isShowProductModal, toggleShowProductModal] = useState(false)
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Carousel className="carousel-style" showStatus={false} showArrows={false}>
+          {product.images.map((image) => (
+            <div className={styles.activeImage}>
+              <img src={image.url} alt={image.name} />
+            </div>
+          ))}
+        </Carousel>
+        <button className={styles.zoomButton} type="button" onClick={() => toggleShowProductModal(!isShowProductModal)}>
+          <FontAwesomeIcon icon={faSearch} />
         </button>
       </div>
+      <div className={styles.panel}>
+        <h1>{product.title}</h1>
+        <p className={styles.description}>{product.description}</p>
+        <p className={styles.price}>{product.price}</p>
+        <div className={styles.btnsContainer}>
+          <button type="button">
+            <Link href={`/products/${product.category}`}>
+              <a>{`Return to ${product.category}`}</a>
+            </Link>
+          </button>
+          <button type="button">
+            <p>Add to cart</p>
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default ProductDetails;
