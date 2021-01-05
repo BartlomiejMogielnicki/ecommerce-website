48c9733329961cff013ccb1754e4a74d6741003c
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-01-05 11:34:46 +0100

    Login/signup form styling

--- components/atoms/Form.tsx
+++ components/atoms/Form.tsx
@@ -6,36 +6,36 @@ interface Props {
 }
 
 const Form:FC<Props> = ({ type }) => (
-  <form>
-    <div>
+  <form className={`${styles.formContainer} ${type === 'login' && styles.small}`}>
+    <div className={styles.inputSection}>
       <label htmlFor="username">
         Username
         <input type="text" id="username" />
       </label>
     </div>
     {type === 'signin' && (
-    <div>
+    <div className={styles.inputSection}>
       <label htmlFor="email">
         E-mail
         <input type="email" id="email" />
       </label>
     </div>
     )}
-    <div>
+    <div className={styles.inputSection}>
       <label htmlFor="password">
         Password
         <input type="password" id="password" />
       </label>
     </div>
     {type === 'signin' && (
-    <div>
+    <div className={styles.inputSection}>
       <label htmlFor="password2">
         Confirm Password
         <input type="password" id="password2" />
       </label>
     </div>
     )}
-    <button type="submit">Submit</button>
+    <button className={styles.submitBtn} type="submit">Submit</button>
   </form>
 )
 
