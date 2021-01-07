cc0777b9093679bc99b88b3df4394a217eebd694
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-01-07 14:00:35 +0100

    Signup/login form inputs validation

--- components/atoms/Form.tsx
+++ components/atoms/Form.tsx
@@ -11,37 +11,82 @@ const Form:FC<Props> = ({ type }) => {
   const [password, setPassword] = useState('')
   const [password2, setPassword2] = useState('')
 
+  const [isUsernameError, toggleUsernameError] = useState(false)
+  const [isEmailError, toggleEmailError] = useState(false)
+  const [isPasswordError, togglePasswordError] = useState(false)
+  const [isPassword2Error, togglePassword2Error] = useState(false)
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    if (username.length === 0) {
+      toggleUsernameError(true)
+      return
+    }
+
+    const validateEmail = (address) => {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(address).toLowerCase());
+    }
+
+    if ((email.length === 0) || (!validateEmail(email))) {
+      toggleEmailError(true)
+      return
+    }
+
+    if (password.length === 0) {
+      togglePasswordError(true)
+      return
+    }
+
+    if (password2.length === 0 || password !== password2) {
+      togglePassword2Error(true)
+      return
+    }
+
+    console.log(`Form submitted with:
+    username: ${username}
+    email: ${email}
+    password: ${password}
+    password2: ${password2}`)
+
+    setUsername('')
+    setEmail('')
+    setPassword('')
+    setPassword2('')
+    toggleUsernameError(false)
+    toggleEmailError(false)
+    togglePasswordError(false)
+    togglePassword2Error(false)
   }
 
   return (
-    <form className={`${styles.formContainer} ${type === 'login' && styles.small}`} onSubmit={(e) => handleSubmit(e)}>
+    <form className={`${styles.formContainer} ${type === 'login' && styles.small}`} noValidate onSubmit={(e) => handleSubmit(e)}>
       <div className={styles.inputSection}>
         <label htmlFor="username">
           Username
-          <input type="text" id="username" onChange={(e) => setUsername(e.target.value)} />
+          <input type="text" maxLength={12} id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
         </label>
       </div>
       {type === 'signin' && (
       <div className={styles.inputSection}>
         <label htmlFor="email">
           E-mail
-          <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+          <input type="email" maxLength={35} id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
       </div>
       )}
       <div className={styles.inputSection}>
         <label htmlFor="password">
           Password
-          <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
+          <input type="password" maxLength={15} id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </label>
       </div>
       {type === 'signin' && (
       <div className={styles.inputSection}>
         <label htmlFor="password2">
           Confirm Password
-          <input type="password" id="password2" onChange={(e) => setPassword2(e.target.value)} />
+          <input type="password" maxLength={15} id="password2" value={password2} onChange={(e) => setPassword2(e.target.value)} />
         </label>
       </div>
       )}
