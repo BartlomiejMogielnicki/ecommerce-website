271740ce4a796f6d0d8c4029da002c2648180889
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-01-13 17:57:46 +0100

    Create products list sort select

--- components/organisms/ProductsList/ProductsList.tsx
+++ components/organisms/ProductsList/ProductsList.tsx
@@ -13,6 +13,18 @@ const ProductsList: FC<Props> = ({ products }) => (
   <div className={styles.container}>
     <div className={styles.categoriesContainer}>
       <Categories />
+      <div className={styles.sortContainer}>
+        <label htmlFor="sort">
+          <h3>Sort</h3>
+          <select name="sort" id="sort">
+            <option value="bestsellers">Bestsellers</option>
+            <option value="nameAtoZ">Name A to Z</option>
+            <option value="nameZtoA">Name Z to A</option>
+            <option value="priceAscending">Price ascending</option>
+            <option value="priceDescending">Price descending</option>
+          </select>
+        </label>
+      </div>
     </div>
     <div className={styles.productsContainer}>
       {products && products.sort((product) => (product.bestseller ? -1 : 1)).map((product) => (
