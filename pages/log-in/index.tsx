616f906e10cdeaef7ee54bc852b5ec67b80adb08
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-01-13 12:04:27 +0100

    Redirect to homepage after log in

--- pages/log-in/index.tsx
+++ pages/log-in/index.tsx
@@ -1,21 +1,34 @@
-import { FC } from 'react';
+import { FC, useContext, useEffect } from 'react';
+import { UserContext } from 'context/UserContext'
+import { useRouter } from 'next/router'
 
 import Header from 'components/organisms/Header';
 import Footer from 'components/atoms/Footer'
 import LoginContent from 'components/organisms/LoginContent/LoginContent'
 
-const Contact: FC = () => (
-  <div>
-    <header>
-      <Header />
-    </header>
-    <main>
-      <LoginContent type="login" />
-    </main>
-    <footer>
-      <Footer />
-    </footer>
-  </div>
-);
+const Contact: FC = () => {
+  const { user: { authenticated } } = useContext(UserContext)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (authenticated) {
+      router.push('/')
+    }
+  }, [authenticated, router])
+
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <LoginContent type="login" />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
 
 export default Contact;
--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,21 +1,34 @@
-import { FC } from 'react';
+import { FC, useContext, useEffect } from 'react';
 
 import Header from 'components/organisms/Header';
 import Footer from 'components/atoms/Footer'
 import LoginContent from 'components/organisms/LoginContent/LoginContent'
+import { UserContext } from 'context/UserContext'
+import { useRouter } from 'next/router'
 
-const Contact: FC = () => (
-  <div>
-    <header>
-      <Header />
-    </header>
-    <main>
-      <LoginContent type="signin" />
-    </main>
-    <footer>
-      <Footer />
-    </footer>
-  </div>
-);
+const Contact: FC = () => {
+  const { user: { authenticated } } = useContext(UserContext)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (authenticated) {
+      router.push('/')
+    }
+  }, [authenticated, router])
+
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <LoginContent type="signin" />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
 
 export default Contact;
