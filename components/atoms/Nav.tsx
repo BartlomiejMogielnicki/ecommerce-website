4918cb70cf72b22dc54aabce0d7c3ec43844f85e
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-01-27 16:12:06 +0100

    Header and navigation small mobiles base styling

--- components/atoms/Nav.tsx
+++ components/atoms/Nav.tsx
@@ -1,27 +1,51 @@
 import { FC } from 'react';
 import Link from 'next/link';
+import {
+  faHome, faShoppingBasket, faBookReader, faPhone,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Nav.module.scss';
 
 const Nav:FC = () => (
   <ul className={styles.list}>
     <li className={styles.item}>
       <Link href="/">
-        <a>Home</a>
+        <a>
+          <p className={styles.itemText}>Home</p>
+          <a className={styles.itemIcon}>
+            <FontAwesomeIcon icon={faHome} />
+          </a>
+        </a>
       </Link>
     </li>
     <li className={styles.item}>
       <Link href="/products">
-        <a>Products</a>
+        <a>
+          <p className={styles.itemText}>Products</p>
+          <a className={styles.itemIcon}>
+            <FontAwesomeIcon icon={faShoppingBasket} />
+          </a>
+        </a>
       </Link>
     </li>
     <li className={styles.item}>
       <Link href="/blog">
-        <a>Blog</a>
+        <a>
+          <p className={styles.itemText}>Blog</p>
+          <a className={styles.itemIcon}>
+            <FontAwesomeIcon icon={faBookReader} />
+          </a>
+        </a>
       </Link>
     </li>
     <li className={styles.item}>
       <Link href="/contact">
-        <a>Contact</a>
+        <a>
+          <p className={styles.itemText}>Contact</p>
+          <a className={styles.itemIcon}>
+            <FontAwesomeIcon icon={faPhone} />
+          </a>
+        </a>
       </Link>
     </li>
   </ul>
