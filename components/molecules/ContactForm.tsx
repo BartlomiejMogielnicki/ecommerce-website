fdbb9ebfd9bc15ab601c6d8ae26bc3aa14a65a60
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-02-28 11:40:01 +0100

    Handle contact form errors

--- components/molecules/ContactForm.tsx
+++ components/molecules/ContactForm.tsx
@@ -6,9 +6,40 @@ const ContactForm:FC = () => {
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
 
+  const [isNameError, setIsNameError] = useState(false)
+  const [isEmailError, setIsEmailError] = useState(false)
+  const [isMessageError, setIsMessageError] = useState(false)
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('Submitted!')
+    let isValidate = true;
+
+    if (!name) {
+      setIsNameError(true)
+      isValidate = false;
+    } else {
+      setIsNameError(false)
+    }
+
+    // E-mail input verification
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if ((!email) || (!re.test(String(email).toLowerCase()))) {
+      setIsEmailError(true)
+      isValidate = false;
+    } else {
+      setIsEmailError(false)
+    }
+
+    if (!message) {
+      setIsMessageError(true)
+      isValidate = false;
+    } else {
+      setIsMessageError(false)
+    }
+
+    if (isValidate) {
+      console.log('Submitted!')
+    }
   }
 
   return (
@@ -25,7 +56,7 @@ const ContactForm:FC = () => {
         <label className={styles.sectionLabel} htmlFor="name">
           Name
           <input
-            className={styles.sectionInput}
+            className={`${styles.sectionInput} ${isNameError && styles.sectionInputError}`}
             type="text"
             name="name"
             id="name"
@@ -35,13 +66,13 @@ const ContactForm:FC = () => {
             value={name}
           />
         </label>
-        <small className={styles.error}>Invalid name</small>
+        {isNameError && <small className={styles.error}>Invalid name</small>}
       </div>
       <div className={styles.section}>
         <label className={styles.sectionLabel} htmlFor="email">
           E-mail
           <input
-            className={styles.sectionInput}
+            className={`${styles.sectionInput} ${isEmailError && styles.sectionInputError}`}
             type="text"
             name="email"
             id="email"
@@ -50,13 +81,13 @@ const ContactForm:FC = () => {
             value={email}
           />
         </label>
-        <small className={styles.error}>Invalid e-mail</small>
+        {isEmailError && <small className={styles.error}>Invalid e-mail</small>}
       </div>
       <div className={styles.section}>
         <label className={styles.sectionLabel} htmlFor="text">
           Message
           <textarea
-            className={styles.sectionTextarea}
+            className={`${styles.sectionTextarea} ${isMessageError && styles.sectionInputError}`}
             name="text"
             id="text"
             placeholder="Enter your message..."
@@ -64,7 +95,7 @@ const ContactForm:FC = () => {
             value={message}
           />
         </label>
-        <small className={styles.error}>Invalid message</small>
+        {isMessageError && <small className={styles.error}>Invalid message</small>}
       </div>
       <button className={styles.submitBtn} type="submit">Submit</button>
     </form>
